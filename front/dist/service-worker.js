0a927aa970e6c2fbb554d6f38dc5304c78d3efdb
Andrew L <mrlogaz@gmail.com>
2019-10-13 23:13:32 +0500

    test

--- front/dist/service-worker.js
+++ front/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.c74cdaf78105bbf52201585520139e0c.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.adbb6b591063053ca6e0d43306796fc4.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
